6bdfbc41c5a1f78476603d55226bbe101e56e060
nsward <wardnick@protonmail.com>
2019-10-04 17:41:01 +0200

    Add constructor arg to storage proxy deployment

--- src/Proxies.t.sol
+++ src/Proxies.t.sol
@@ -4,22 +4,25 @@ import "ds-test/test.sol";
 
 import "./ProxyCode.sol";
 import "./Factory.sol";
+import "./Logic.sol";
 
 contract ProxiesTest is DSTest, Factory, HuffProxies {
 
+    address internal logic;
     address internal proxyConstant;
     address internal proxyStorage;
     address internal proxy2d;
 
     function setUp() public {
+        logic = address(new Logic());
         proxyConstant = deploy(proxyConstant_init);
-        // proxyStorage = deploy(proxyStorage_init);
-        // proxy2d = deploy(proxy2d_init);
+        proxyStorage = deploy(abi.encodePacked(proxyStorage_init, bytes32(uint256(logic))));
+        // proxy2d = deploy(abi.encodePacked(proxy2d_init, bytes32(uint256(logic))));
     }
 
     function testDeployment() public {
         assertEq(proxyConstant_hash, codehash(proxyConstant));
-        // assertEq(proxyStorage_hash, codehash(proxyStorage));
+        assertEq(proxyStorage_hash, codehash(proxyStorage));
         // assertEq(proxy2d_hash, codehash(proxy2d));
     }
 
